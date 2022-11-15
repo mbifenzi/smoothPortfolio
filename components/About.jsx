bd4abf0921ecdfab10708c2d83bf6d4cd75d13dc
Mohamed Bifenzi <mbifenzi@e2r2p9.1337.ma>
2022-11-15 04:06:15 +0100

    khribi9aaat

--- components/About.jsx
+++ components/About.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import Lottie from "react-lottie";
 import animationData from "../assets/lottie/face.json";
 import { motion as m } from 'framer-motion';
+import Image from 'next/image'
+
+
+import {FaGit, FaReact} from 'react-icons/fa'
+import {FaNodeJs} from 'react-icons/fa'
+import {FaDocker} from 'react-icons/fa'
+import {FaJs} from 'react-icons/fa'
+import {SiNestjs} from 'react-icons/si'
+import {TbBrandNextjs} from 'react-icons/tb'
+
+
+
+
 
 function CoronaVirus() {
   const defaultOptions = {
@@ -12,23 +25,38 @@ function CoronaVirus() {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
-
   return <Lottie options={defaultOptions} height={400} width={400} />;
 }
 
 const About = () => {
   return (
-    <m.div initial={{opacity: 0}} animate={{opacity: 1}}>
-      <div className='w-full h-screen flex flex-col sm:flex-row justify-center items-center pt-36 sm:pt-0 bg-pink-100'>
-        <div className=" p-6 sm:w-1/2 w-full" id='about'>
-          <div className='w-fit'>
-            <h1 className="text-8xl font-bold font-[bifHeader] text-left-10 text-justify">HELLO I AM BIFENZI</h1>
-            <h5 className='text-right font-[bifParagraph]'>mohamed bifenzi</h5>
+    <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <div id='about' className='w-full h-fit sm:h-screen flex flex-col justify-center items-center bg-pink-100'>
+        <div className='  flex flex-col sm:flex-row justify-center items-center pt-36 sm:pt-0'>
+          <div className=" p-6 sm:w-1/2 w-full" >
+            <div className='w-fit pb-10'>
+              <h1 className="text-8xl font-bold font-[bifHeader] text-left-10 text-justify">HELLO I AM BIFENZI</h1>
+              <h5 className='text-right font-[bifParagraph]'>mohamed bifenzi</h5>
+            </div>
+            <h1 className=" md:text-xl sm:text-lg text-left font-normal">I AM A FRONT-END DEVELOPER AND UX/UI DESIGNER AND A STUDENT AT @1337 LOCATED IN MOROCCO, I THOROUGHLY ENJOY CREATING NEW THINGS AND I AM ALWAYS LOOKING FOR NEW CHALLENGES, SEEING IDEAS AND CONCEPTS COME TO LIFE IS ONE OF MY FAVORITE PARTS OF THE DEVELOPMENT PROCESS.</h1>
           </div>
-          <h1 className=" md:text-xl sm:text-lg text-justify font-medium ">I AM A FRONT-END DEVELOPER AND UX/UI DESIGNER AND A STUDENT AT @1337 LOCATED IN MOROCCO, I THOROUGHLY ENJOY CREATING NEW THINGS AND I AM ALWAYS LOOKING FOR NEW CHALLENGES, SEEING IDEAS AND CONCEPTS COME TO LIFE IS ONE OF MY FAVORITE PARTS OF THE DEVELOPMENT PROCESS.</h1>
+          <div className="w-full sm:w-1/2 h-full flex justify-center items-center">
+            <CoronaVirus />
+          </div>
         </div>
-        <div className="w-full sm:w-1/2 h-full flex justify-center items-center">
-          <CoronaVirus />
+        <div className='py-10'>
+          <h1 className='font-bold text-center'>
+            TOOLS AND TECHNOLOGIES I USE:
+          </h1>
+          <div className='flex flex-wrap justify-center gap-6 py-10'>
+            <FaDocker size={"5em"}  className="border-black-100 bg-black bg-opacity-10 hover:bg-opacity-25 transition duration-300 rounded-2xl shadow-2xl -full p-3"/>
+            <FaJs size={"5em"} className="border-black-100 bg-black bg-opacity-10 hover:bg-opacity-25 transition duration-300 shadow-2xl rounded-2xl p-3"/>
+            <FaNodeJs size={"5em"} className="border-black-100 bg-black bg-opacity-10 hover:bg-opacity-25 transition duration-300 shadow-2xl rounded-2xl p-3"/>
+            <SiNestjs size={"5em"} className="border-black-100 bg-black bg-opacity-10 hover:bg-opacity-25 transition duration-300 shadow-2xl rounded-2xl p-3"/>
+            <FaReact size={"5em"} className="border-black-100 bg-black bg-opacity-10 hover:bg-opacity-25 transition duration-300 shadow-2xl rounded-2xl p-3"/>
+            <TbBrandNextjs size={"5em"} className="border-black-100 bg-black bg-opacity-10 hover:bg-opacity-25 transition duration-300 shadow-2xl rounded-2xl p-3"/>
+            <FaGit size={"5em"} className="border-black-100 bg-black bg-opacity-10 hover:bg-opacity-25 transition duration-300 shadow-2xl rounded-2xl p-3"/>
+          </div>
         </div>
       </div>
     </m.div>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,9 +1,108 @@
 import React from 'react'
+import { FaReact } from 'react-icons/fa'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const listOfProjects = [
+  {
+    name: 'ft-containers',
+    description: 'I reimplemented some C++ std containers using dynamic array and red black tree.',
+    image: '/assets/projects/containers.jpeg',
+    link: '',
+    github: '',
+    technologies: ["C++", "DATA STRUCTURE", "ALGORITHM"],
+  },
+  {
+    name: 'Cub3D',
+    description: 'This is my first raycasting project, I made it with C',
+    image: '/assets/projects/containers.jpeg',
+    link: '',
+    github: '',
+    technologies: ["C", "Minilibx"],
+  },
+  {
+    name: 'inception',
+    description: 'i Set up a small infrastructure composed of WordPress with LEMP Stack, (Nginx, MariaDB and PHP) using Docker / Docker-Compose.',
+    image: '/assets/projects/containers.jpeg',
+    link: '',
+    github: '',
+    technologies: ["Docker", "Docker-Compose"],
+  },
+  {
+    name: 'MiniShell',
+    description: 'this is a simple shell implementation, I made it with C',
+    image: '/assets/projects/containers.jpeg',
+    link: '',
+    github: '',
+    technologies: ["C", "UNIX"],
+  },
+  {
+    name: 'Push_swap',
+    description: 'This is a sorting algorithm project, I made it with C',
+    image: '/assets/projects/containers.jpeg',
+    link: '',
+    github: '',
+    technologies: ["C", "Sorting Algorithms"],
+  },
+  {
+    name: 'WEBSERV',
+    description: 'A three man project. We wrote our own Http server form scratch using C++.',
+    image: '/assets/projects/containers.jpeg',
+    link: '',
+    github: '',
+    technologies: ["C++", "SOCKETS"],
+  },
+  {
+    name: 'ft-transcendence',
+    description: 'A 42 project. We wrote a full stack web application using React, Node.js, and PostgreSQL.',
+    image: '/assets/projects/containers.jpeg',
+    link: '',
+    github: '',
+    technologies: ["React", "Node.js", "PostgreSQL", "TypeScript", "Socket.io"],
+  },
+  {
+    name: 'Philosophers',
+    description: 'Three different implementation of the famous dining philosophers problem using mutex and threads',
+    image: '/assets/projects/containers.jpeg',
+    link: '',
+    github: '',
+    technologies: ["C", "Threads", "Mutex"],
+  },
+]
+
 
 const Projects = () => {
   return (
-    <div className='h-screen bg-red-100'>
-      <h1>Projects</h1>
+    <div className='h-fit md:h-sreen bg-red-100'>
+      <div className='flex justify-center items-center'>
+        <h1 className='text-2xl'>SOME OF MY PROJECTS</h1>
+      </div>
+      <div className='container mx-auto flex justify-center items-center'>
+        
+        <div className='flex flex-wrap sm:flex-row justify-center items-center '>
+          {listOfProjects.map((project) => (
+              <Link href={"/alo"} className="border-black border-2 bg-pink-100 rounded-xl m-5 sm:w-2/5 w-2/3 md:w-1/4 h-96  text-justify">
+              <div className=''>
+                <Image src={project.image} alt='project' width={300} height={500} className="rounded-t-lg"/>
+                <h1 className='font-bold text-center border-black border-b-2'>
+                  {project.name}
+                </h1>
+
+                <div className=''>
+                 <h1 className=''>Skills used</h1>  
+                  {project.technologies.map((tech) => (
+                    <div className='m-2'>
+                      <p className=' text-gray-500'>
+                        {tech}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
