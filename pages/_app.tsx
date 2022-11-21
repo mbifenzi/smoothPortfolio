736baa2e6ee84a6f2d9db1b8e74d400096bfecfb
mohamed bifenzi <mbifenzi@bifenzi.local>
2022-11-21 03:59:59 +0100

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Head } from 'next/document'
+// import { Head } from 'next/document'
 
 export default function App({ Component, pageProps }: AppProps) {
 
