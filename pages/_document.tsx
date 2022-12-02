e98eaf3e3253df024ef17d0eb9128aa8892342c9
mohamed bifenzi <mbifenzi@bifenzi.local>
2022-12-02 03:47:41 +0100

    blank space at right solved

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html className='scroll-smooth' style={{scrollBehavior:'smooth'}}>
+                <Head>
+                    <title>MBifenzi</title>
+                    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,17 @@
-// import styles from '../styles/Home.module.css'
-
 import About from '../components/About'
 import Header from '../components/Header'
 import Projects from '../components/Projects.jsx'
 import Contact from '../components/Contact'
-import Head from 'next/head'
+
 
 
 export default function Home() {
   return (
-    <div className='overflow-scroll'>
-      <Head>
-        <title>MBifenzi</title>
-        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-      </Head>
-      <div className='snap-y snap-mandatory overflow-scroll '>    
+    <div className='scroll-smooth' style={{scrollBehavior:'smooth'}}>
       <Header />
       <About />
       <Projects />
       <Contact />
-      </div>
-
     </div>
   )
 }
