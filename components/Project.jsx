bbdd5450060ea4b9cd02c5e0ee2acedb7c3395c3
Mohamed Bifenzi <mbifenzi@e2r2p3.1337.ma>
2022-12-03 21:11:09 +0100

    project component loading

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,29 +1,161 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { motion as m } from 'framer-motion'
-import animationData from "../assets/lottie/contact.json";
 import Lottie from "react-lottie";
 
 
-function CoronaVirus() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  return <Lottie options={defaultOptions} height={400} width={400} />;
-}
+const listOfProjects = [
+  {
+    id: 1,
+    name: "ft-containers",
+    description:
+      "I reimplemented some C++ std containers using dynamic array and red black tree.",
+    image: "/assets/projects/containers.jpeg",
+    link: "",
+    github: "",
+    technologies: ["C++", "DATA STRUCTURE", "ALGORITHM"],
+  },
+  {
+    id: 2,
+    name: "Cub3D",
+    description: "This is my first raycasting project, I made it with C",
+    image: "/assets/projects/cub3d.png",
+    link: "",
+    github: "",
+    technologies: ["C", "Minilibx"],
+  },
+  {
+    id: 3,
+    name: "inception",
+    description:
+      "i Set up a small infrastructure composed of WordPress with LEMP Stack, (Nginx, MariaDB and PHP) using Docker / Docker-Compose.",
+    image: "/assets/projects/inception.jpeg",
+    link: "",
+    github: "",
+    technologies: ["Docker", "Docker-Compose"],
+  },
+  {
+    id: 4,
+    name: "MiniShell",
+    description: "this is a simple shell implementation, I made it with C",
+    image: "/assets/projects/minishell.png",
+    link: "",
+    github: "",
+    technologies: ["C", "UNIX"],
+  },
+  {
+    id: 5,
+    name: "Push_swap",
+    description: "This is a sorting algorithm project, I made it with C",
+    image: "/assets/projects/pushswap.png",
+    link: "",
+    github: "",
+    technologies: ["C", "Sorting Algorithms"],
+  },
+  {
+    id: 6,
+    name: "WEBSERV",
+    description:
+      "A three man project. We wrote our own Http server form scratch using C++.",
+    image: "/assets/projects/webserv.jpeg",
+    link: "",
+    github: "",
+    technologies: ["C++", "SOCKETS"],
+  },
+  {
+    id: 7,
+    name: "ft-transcendence",
+    description:
+      "A 42 project. We wrote a full stack web application using React, Node.js, and PostgreSQL.",
+    image: "/assets/projects/ft-transcandante.png",
+    link: "",
+    github: "",
+    technologies: ["React", "Node.js", "PostgreSQL", "TypeScript", "Socket.io"],
+  },
+  {
+    id: 8,
+    name: "Philosophers",
+    description:
+      "Three different implementation of the famous dining philosophers problem using mutex and threads",
+    image: "/assets/projects/philo.webp",
+    link: "",
+    github: "",
+    technologies: ["C", "Threads", "Mutex"],
+  },
+];
 
-
-
+// const activClass = "card grid grid-cols-1 grid-rows-1 gap-4 order-none h-auto";
+// const inactiveClass = "card col-span-2 row-span-2 order-1 h-100% ";
 const Project = () => {
-  return (
-    <div data-aos="fade-right" className='container bg-red-400 mx-auto h-screen snap-start w-full flex justify-center items-center'>
-      <CoronaVirus />
-    </div>
-  )
-}
+  const [isClicked, setIsClicked] = React.useState(false);
+  function handleClick(id) {
+    setIsClicked(!isClicked);
+  }
+  useEffect(() => {
+    const cards = document.querySelectorAll(".card");
+    const inactiveClasses = document.querySelectorAll(".inactive");
+    cards.forEach((card,  index) => {
+      card.addEventListener("click", () => {
+        // const state = Flip.getState(card);
+        const isCardActive = card.classList.contains("active");
+        cards.forEach((otherCard, otherIndex) => {
+          otherCard.classList.remove("active");
+          otherCard.classList.remove("inactive");
+      });
+        if (!isCardActive) {
+          card.classList.add("active");
+        }
+        });
+    });
+  }, [isClicked]);
 
+  return (
+    <section className='container'>
+      {/* <div onClick={() => handleClick(0)} className="card inactive">
+        <h1 className='text-2xl font-bold text-center'>{listOfProjects[0].name}</h1>
+        <img src={listOfProjects[0].image} alt='project' />
+        <p className='text-center'>{listOfProjects[0].description}</p>
+      </div>
+      <div onClick={() => handleClick(1)} className="card inactive">
+        <h1 className='text-2xl font-bold text-center'>{listOfProjects[1].name}</h1>
+        <img src={listOfProjects[1].image} alt='project' />
+        <p className='text-center'>{listOfProjects[1].description}</p>
+      </div>
+      <div onClick={() => handleClick(2)} className="card inactive">
+        <h1 className='text-2xl font-bold text-center'>{listOfProjects[2].name}</h1>
+        <img src={listOfProjects[2].image} alt='project' />
+        <p className='text-center'>{listOfProjects[2].description}</p>
+      </div>
+      <div onClick={() => handleClick(3)} className="card inactive">
+        <h1 className='text-2xl font-bold text-center'>{listOfProjects[3].name}</h1>
+        <img src={listOfProjects[3].image} alt='project' />
+        <p className='text-center'>{listOfProjects[3].description}</p>
+      </div>
+      <div onClick={() => handleClick(4)} className="card inactive">
+        <h1 className='text-2xl font-bold text-center'>{listOfProjects[4].name}</h1>
+        <img src={listOfProjects[4].image} alt='project' />
+        <p className='text-center'>{listOfProjects[4].description}</p>
+        </div>
+      <div onClick={() => handleClick(5)} className="card inactive">
+        <h1 className='text-2xl font-bold text-center'>{listOfProjects[5].name}</h1>
+        <img src={listOfProjects[5].image} alt='project' />
+        <p className='text-center'>{listOfProjects[5].description}</p>
+        </div>
+      <div onClick={() => handleClick(6)} className="card inactive">
+        <h1 className='text-2xl font-bold text-center'>{listOfProjects[6].name}</h1>
+        <img src={listOfProjects[6].image} alt='project' />
+        <p className='text-center'>{listOfProjects[6].description}</p>
+      </div>
+      <div onClick={() => handleClick(7)} className="card inactive">
+        <h1 className='text-2xl font-bold text-center'>{listOfProjects[7].name}</h1>
+        <img src={listOfProjects[7].image} alt='project' />
+        <p className='text-center'>{listOfProjects[7].description}</p>
+      </div> */}
+    <div className='card' onClick={() =>handleClick(0)} >test</div>
+    <div className='card' onClick={() =>handleClick(1)}>test1</div>
+    <div className='card' onClick={() =>handleClick(3)}>test2</div>
+    <div className='card' onClick={() =>handleClick(4)} >test3</div>
+    </section>
+  );
+};
 export default Project
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import About from '../components/About'
 import Header from '../components/Header'
 import Projects from '../components/Projects.jsx'
 import Contact from '../components/Contact'
-
+import Project from '../components/Project'
 
 
 export default function Home() {
@@ -12,6 +12,7 @@ export default function Home() {
       <About />
       <Projects />
       <Contact />
+      {/* <Project /> */}
     </div>
   )
 }
